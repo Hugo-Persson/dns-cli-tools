534d501b69caa06f70d1e80d2d7b32e8ac327e6a
Hugo Persson <hugo.e.persson@gmail.com>
2025-04-06 13:59:00 +0200

    release: bump version and generate changelog

--- scripts/do-release.sh
+++ scripts/do-release.sh
@@ -2,21 +2,21 @@
 
 # cd to the root of the project
 
-cd $(dirname $0)/..
+cd $(dirname $0)/.. || exit
 
 old_version=$(git describe --tags --abbrev=0)
 
 echo "Old version: $old_version"
 
-NEW_VERSION=$(semver -i patch $old_version)
+NEW_VERSION=$(semver -i patch "$old_version")
 echo "New version: $NEW_VERSION"
 sed -i '' "s/^version = ".*"/version = \"$NEW_VERSION\"/" ./Cargo.toml
 cargo build
+git cliff --tag "$NEW_VERSION" -o CHANGELOG.md
+git add CHANGELOG.md
 git add ./Cargo.*
 git commit -m "Release $NEW_VERSION"
-git tag $NEW_VERSION
+git tag "$NEW_VERSION"
 
-git cliff -o CHANGELOG.md
-git add CHANGELOG.md
 git push
 git push --tags
